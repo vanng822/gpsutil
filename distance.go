ecc289b527d1379f200ec0cdeaaba7ce24bfaa49
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-09-07 13:50:34 +0200

    Adding GetMidPoint

--- distance.go
+++ distance.go
@@ -1,18 +1,19 @@
 package gpsutil
 
 import (
+	"fmt"
 	"math"
 )
 
 func GetDistance(lng1, lat1, lng2, lat2 float64) float64 {
 	dLat := toRad(lat2 - lat1)
 	dLng := toRad(lng2 - lng1)
-	a := math.Pow(math.Sin(dLat/2), 2) + math.Pow(math.Sin(dLng/2), 2) * math.Cos(toRad(lat1))*math.Cos(toRad(lat2))
+	a := math.Pow(math.Sin(dLat/2), 2) + math.Pow(math.Sin(dLng/2), 2)*math.Cos(toRad(lat1))*math.Cos(toRad(lat2))
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 	return c * EARTH_RADIUS
 }
 
-func GetTotalDistance(points []LatLng) float64 {
+func GetTotalDistance(points []*LatLng) float64 {
 	max := len(points) - 1
 	total := 0.0
 	for i := 0; i < max; i++ {
@@ -26,6 +27,33 @@ func GetBoundingBox(lat, lng, distance float64) *BBox {
 	return &BBox{}
 }
 
-func GetMidPoint(points []LatLng) (*LatLng, error) {
-	return nil, nil
+func GetMidPoint(points []*LatLng) (*LatLng, error) {
+	length := len(points)
+	x := 0.0
+	y := 0.0
+	z := 0.0
+
+	if length < 1 {
+		return nil, fmt.Errorf("Points must not be empty")
+	} else if length == 1 {
+		return &LatLng{lat: points[0].lat, lng: points[0].lng}, nil
+	}
+	var lat, lng float64
+
+	for i := 0; i < length; i++ {
+		lat = toRad(points[i].lat)
+		lng = toRad(points[i].lng)
+		x += math.Cos(lat) * math.Cos(lng)
+		y += math.Cos(lat) * math.Sin(lng)
+		z += math.Sin(lat)
+	}
+
+	x = x / float64(length)
+	y = y / float64(length)
+	z = z / float64(length)
+
+	lng = math.Atan2(y, x)
+	lat = math.Atan2(z, math.Sqrt(x*x+y*y))
+
+	return &LatLng{lat: toDegrees(lat), lng: toDegrees(lng)}, nil
 }
--- distance_test.go
+++ distance_test.go
@@ -14,11 +14,11 @@ func TestGetDistance(t *testing.T) {
 
 func TestGetTotalDistance(t *testing.T) {
 	expected := 60.29237674121895
-	points := make([]LatLng, 4)
-	points[0] = LatLng{lat: 59.19305333867669, lng: 17.661922238767147}
-	points[1] = LatLng{lat:59.192982176318765 , lng: 17.662122901529074}
-	points[2] = LatLng{lat: 59.19288511388004, lng: 17.66255029477179}
-	points[3] = LatLng{lat: 59.19290036894381, lng: 17.662896132096648}
+	points := make([]*LatLng, 4)
+	points[0] = &LatLng{lat: 59.19305333867669, lng: 17.661922238767147}
+	points[1] = &LatLng{lat:59.192982176318765 , lng: 17.662122901529074}
+	points[2] = &LatLng{lat: 59.19288511388004, lng: 17.66255029477179}
+	points[3] = &LatLng{lat: 59.19290036894381, lng: 17.662896132096648}
 	result := GetTotalDistance(points)
 	if result != expected {
 		t.Errorf("Expected '%v' but got '%v'", expected, result)
@@ -27,9 +27,49 @@ func TestGetTotalDistance(t *testing.T) {
 
 func TestGetTotalDistanceZero(t *testing.T) {
 	expected := 0.0
-	points := make([]LatLng, 0)
+	points := make([]*LatLng, 0)
 	result := GetTotalDistance(points)
 	if result != expected {
 		t.Errorf("Expected '%v' but got '%v'", expected, result)
+	}
+}
+func TestGetMidPointNone(t *testing.T) {
+	points := make([]*LatLng, 0)
+	
+	_, err := GetMidPoint(points)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+	
+}
+
+func TestGetMidPointOne(t *testing.T) {
+	expected := &LatLng{lat: 59.19290036894381, lng: 17.662896132096648}
+	points := make([]*LatLng, 1)
+	points[0] = &LatLng{lat: 59.19290036894381, lng: 17.662896132096648}
+	
+	result, _ := GetMidPoint(points)
+	if result.Lng() != expected.Lng() {
+		t.Errorf("Expected '%v' but got '%v'", expected.Lng(), result.Lng())
+	}
+	
+	if result.Lat() != expected.Lat() {
+		t.Errorf("Expected '%v' but got '%v'", expected.Lat(), result.Lat())
+	}
+}
+
+func TestGetMidPointMultiPoints(t *testing.T) {
+	expected := &LatLng{lat: 59.3000383089087, lng: 17.299706052455726}
+	points := make([]*LatLng, 2)
+	points[0] = &LatLng{lat: 59.2, lng: 17.2}
+	points[1] = &LatLng{lat: 59.4, lng: 17.4}
+	
+	result, _ := GetMidPoint(points)
+	if result.Lng() != expected.Lng() {
+		t.Errorf("Expected '%v' but got '%v'", expected.Lng(), result.Lng())
+	}
+	
+	if result.Lat() != expected.Lat() {
+		t.Errorf("Expected '%v' but got '%v'", expected.Lat(), result.Lat())
 	}
 }
