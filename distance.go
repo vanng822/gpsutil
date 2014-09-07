26a3af0a93972294edb97e18edac1e66f0c11a62
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-09-07 11:56:08 +0200

    Implement distance functions

--- distance.go
+++ distance.go
@@ -1,8 +1,15 @@
 package gpsutil
 
-func GetDistance(lng1, lat1, lng2, lat2 float64) float64 {
+import (
+	"math"
+)
 
-	return 0.0
+func GetDistance(lng1, lat1, lng2, lat2 float64) float64 {
+	dLat := toRad(lat2 - lat1)
+	dLng := toRad(lng2 - lng1)
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) + math.Sin(dLng/2)*math.Sin(dLng/2)*math.Cos(toRad(lat1))*math.Cos(toRad(lat2))
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return c * EARTH_RADIUS
 }
 
 func GetTotalDistance(points []LatLng) float64 {
@@ -15,10 +22,10 @@ func GetTotalDistance(points []LatLng) float64 {
 	return total
 }
 
-func GetBoundingBox(lat, lng, distance float64) BBox {
-	return nil
+func GetBoundingBox(lat, lng, distance float64) *BBox {
+	return &BBox{}
 }
 
-func GetMidPoint(points []LatLng) (LatLng, error) {
-	return nil
+func GetMidPoint(points []LatLng) (*LatLng, error) {
+	return nil, nil
 }
---
+++ distance_test.go
@@ -0,0 +1,27 @@
+package gpsutil
+
+import (
+	"testing"
+)
+
+func TestGetDistance(t *testing.T) {
+	expected := 13.899604253423052
+	result := GetDistance(17.661922238767147, 59.19305333867669, 17.662122901529074, 59.192982176318765)
+	if result != expected {
+		t.Errorf("Expected '%v' but got '%v'", expected, result)
+	}
+}
+
+func TestGetTotalDistance(t *testing.T) {
+	expected := 60.29237674121895
+	points := make([]LatLng, 4)
+	points[0] = LatLng{lat: 59.19305333867669, lng: 17.661922238767147}
+	points[1] = LatLng{lat:59.192982176318765 , lng: 17.662122901529074}
+	points[2] = LatLng{lat: 59.19288511388004, lng: 17.66255029477179}
+	points[3] = LatLng{lat: 59.19290036894381, lng: 17.662896132096648}
+	result := GetTotalDistance(points)
+	if result != expected {
+		t.Errorf("Expected '%v' but got '%v'", expected, result)
+	}
+		
+}
